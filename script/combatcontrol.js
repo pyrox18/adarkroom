7e82673e6604669a887b80b336899d8de6202913
zafum5 <mfaishal.dzaky@gmail.com>
2019-01-27 01:57:37 +0800

    Remove debug log

--- script/combatcontrol.js
+++ script/combatcontrol.js
@@ -45,19 +45,15 @@ function bindCombatKeys() {
     $('body').bind('keypress.combat', function(e) {
         switch(e.keyCode){
             case combatKeys.stab.keyCode:
-                console.log("Button " + combatKeys.stab.text + " pressed");
                 $("#attack_bone-spear").trigger("click");
                 break;
             case combatKeys.swing.keyCode:
-                console.log("Button " + combatKeys.swing.text + " pressed");
                 $("#attack_iron-sword").trigger("click");
                 break;
             case combatKeys.slash.keyCode:
-                console.log("Button " + combatKeys.slash.text + " pressed");
                 $("#attack_steel-sword").trigger("click");
                 break;
             case combatKeys.thrust.keyCode:
-                console.log("Button " + combatKeys.thrust.text + " pressed");
                 $("#attack_bayonet").trigger("click");
                 break;
             case combatKeys.eat.keyCode:
